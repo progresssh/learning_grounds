86e93a547ac876473dceab37533ee869d3937fc7
star <star@progress.sh>
2026-01-03 23:47:06 +0100

    sine wave enemy spawn

--- main.rs
+++ main.rs
@@ -1,12 +1,18 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(TilemapPlugin)
         .add_systems(Startup, setup_camera)
         .add_systems(Startup, setup_player)
         .add_systems(Update, move_player)
         .add_systems(Update, build_tower)
+        .add_systems(Update, spawn_enemy)
+        .add_systems(Update, move_enemy)
         .run();
 }
 
@@ -17,11 +23,14 @@ fn setup_camera(mut commands: Commands) {
 #[derive(Component)]
 struct Player;
 
-fn setup_player(mut commands: Commands) {
+fn setup_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+ ) {
     commands.spawn((
         Sprite {
-            color: Color::srgb(0.0, 0.0, 1.0),
-            custom_size: Some(Vec2::new(50.0, 50.0)),
+            image: asset_server.load("player.png"),
+            custom_size: Some(Vec2::new(48.0, 48.0)),
             ..default()
         },
         Player,
@@ -62,20 +71,21 @@ fn move_player(
 #[derive(Component)]
 struct Tower;
 
-fn build_tower (
+fn build_tower(
     mut commands: Commands,
     time: Res<Time>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
+    asset_server: Res<AssetServer>,
     mut query: Query<&mut Transform, With<Player>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Space) {
         if let Ok(player_transform) = query.single() {
             let player_pos = player_transform.translation;
-            
+
             commands.spawn((
                 Sprite {
-                    color: Color::srgb(1.0, 1.0, 0.0),
-                    custom_size: Some(Vec2::new(25.0, 25.0)),
+                    image: asset_server.load("tower.png"),
+                    custom_size: Some(Vec2::new(48.0, 48.0)),
                     ..default()
                 },
                 Transform::from_translation(player_pos),
@@ -83,4 +93,55 @@ fn build_tower (
             ));
         }
     }
+}
+
+fn defense_tower (
+    mut commands: Commands,
+    mut query: Query<&mut Transform, With<Player>>,
+) {
+
+}
+
+#[derive(Component)]
+struct Enemy;
+
+struct EnemySpawner {
+    timer: Timer,
+}
+
+impl Default for EnemySpawner {
+    fn default() -> Self {
+        Self {
+            timer: Timer::from_seconds(0.5, TimerMode::Repeating),
+        }
+    }
+}
+
+fn spawn_enemy(
+    mut commands: Commands,
+    time: Res<Time>,
+    asset_server: Res<AssetServer>,
+    mut spawner: Local<EnemySpawner>, 
+) {
+    spawner.timer.tick(time.delta());
+    if spawner.timer.is_finished() {
+        commands.spawn((
+            Sprite {
+                image: asset_server.load("enemy.png"),
+                ..default()
+            },
+            Transform::from_xyz(-700.0, 0.0, 0.0),
+            Enemy,
+        ));
+    }
+}
+
+fn move_enemy (
+    time: Res<Time>,
+    mut enemies: Query<&mut Transform, With<Enemy>>,
+) {
+    for mut transform in &mut enemies {
+        transform.translation.x += (time.delta_secs() * 10.0);
+        transform.translation.y += ((transform.translation.x + PI * time.delta_secs()).sin() * 100.0) * (5.0 * time.delta_secs());
+    } 
 }
---
+++ notes/sound.md
@@ -0,0 +1,3 @@
+Sounds of tower at intervals, such that each time of tower, playing a different note, play it as though they are talking to each other
+
+
